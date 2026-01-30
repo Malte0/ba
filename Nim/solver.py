6aa860f4a8ee9a2fce6bec807587cf02657a258b
Malte Wagenknecht <wagenknecht.malte@gmail.com>
2026-01-30 12:44:54 +0100

    I get a result at least

--- Nim/solver.py
+++ Nim/solver.py
@@ -1,24 +1,35 @@
 
 from game import create_T
 
-def payoff(n, T, max_steps):
-    look_up_table = {}
-    for step in range(1, max_steps):
-        ts1 = T[n-step][0]
-        ts2 = T[n-step][1]
-        # (position, is_player_turn, tsi) = payoff
-        look_up_table[(n-step, True, ts1)] = 1 if n-step+ts1 >= n else min(look_up_table[(n-step+ts1, False, ts1)], look_up_table[(n-step+ts1, False, ts2)])
-        look_up_table[(n-step, True, ts2)] = 1 if n-step+ts2 >= n else min(look_up_table[(n-step+ts2, False, ts1)], look_up_table[(n-step+ts2, False, ts2)])
-        look_up_table[(n-step, False, ts1)] = 0 if n-step+ts1 >= n else max(look_up_table[(n-step+ts1, True, ts1)], look_up_table[(n-step+ts1, True, ts2)])
-        look_up_table[(n-step, False, ts2)] = 0 if n-step+ts2 >= n else max(look_up_table[(n-step+ts2, True, ts1)], look_up_table[(n-step+ts2, True, ts2)])
+lut = {}
 
-    return look_up_table
+def payoff(N, T, step, max_steps):
+    Ts0 = T[step][0]
+    Ts1 = T[step][1]
+    
+    # N-step to N-1
+    for i in range(1, step+1):
+        # (step, ns, Tchoice, myTurn)
+        lut[(step, N-i, Ts0, True)] = 1 if N-i+Ts0 >= N else min([lut[(step-1, N-i+Ts0, T[step-1][0], False)], lut[(step-1, N-i+Ts0, T[step-1][1], False)]])
+        lut[(step, N-i, Ts0, False)] = 0 if N-i+Ts0 >= N else max([lut[(step-1, N-i+Ts0, T[step-1][0], True)], lut[(step-1, N-i+Ts0, T[step-1][1], True)]])
+        
+        lut[(step, N-i, Ts1, True)] = 1 if N-i+Ts1 >= N else min([lut[(step-1, N-i+Ts1, T[step-1][0], False)], lut[(step-1, N-i+Ts1, T[step-1][1], False)]])
+        lut[(step, N-i, Ts1, False)] = 0 if N-i+Ts1 >= N else max([lut[(step-1, N-i+Ts1, T[step-1][0], True)], lut[(step-1, N-i+Ts1, T[step-1][1], True)]])
+    
+    if step == max_steps:
+        return
+    payoff(N, T, step+1, max_steps)
 
-def create_lookup_table(n, T, max_steps):
-    lookup_table = payoff(n, T, max_steps)
-    return lookup_table
+
+
+def create_lookup_table(N, T, max_steps):
+    lut.clear()
+    payoff(N, T, 1, max_steps)
+    return lut
 
 if __name__ == "__main__":
+    N = 23
+    K = 5
     MAX_STEPS = 3
-    N, K, T, s = create_T(23, 5)
+    T = create_T(N, K)
     print(create_lookup_table(N, T, MAX_STEPS))
