22fb84ee85abd16dab883da0ffe8bbc150327cc6
Malte Wagenknecht <etlamwag@gmail.com>
2026-02-18 10:24:58 +0100

    more skyjo

--- skyjo/main.py
+++ skyjo/main.py
@@ -2,12 +2,22 @@
 from deck import Card_Deck
 from game import Game
 from player import Player
+import random
+
+def open_random_card(player: Player):
+    closed_cards = []
+    for row_index in range(3):
+        for column_index in range(4):
+            if not player.card_grid[row_index][column_index].is_open():
+                closed_cards.append([row_index, column_index])
+
+    random_card = random.choice(closed_cards)
+    player.open_card(random_card[0],random_card[1])
 
 if __name__ == "__main__":
     print("start")
     deck = Card_Deck()
     player = Player(deck)
-    player.open_card(0,0)
-    player.open_card(2,3)
-    player.open_card(1,2)
+    open_random_card(player)
+    open_random_card(player)
     player.print_grid()
